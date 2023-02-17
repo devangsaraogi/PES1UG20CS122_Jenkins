8d117a5fdc8c75b7ad0099ff1b896ea0be9e8e0f
devangsaraogi <70952871+devangsaraogi@users.noreply.github.com>
2023-02-17 10:32:02 +0530

    Automating Webhooks
    demonstrating working of webhooks

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello World [PES1UG20CS122]\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  
+  cout << "Automating Build using Jenkins";
   return 0;
 }
